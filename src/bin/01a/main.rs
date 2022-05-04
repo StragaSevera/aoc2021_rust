5f7d66efbba403ca3a777bda9450d28fd656ee27
StragaSevera <ought@yandex.ru>
2022-05-05 00:25:33 +0300

    Solve 01a

---
+++ src/bin/01a/main.rs
@@ -0,0 +1,59 @@
+fn main() {
+    let input = aoc2021::read_file("src/bin/01a/input.txt");
+    let result = calibrate(input);
+    println!("{}", result);
+}
+
+fn calibrate<S: AsRef<str>>(input: impl Iterator<Item=S>) -> i32 {
+    enum CalibrationAcc {
+        Empty,
+        Full(i32, i32),
+    }
+
+    let numbers = input.map(|line| line.as_ref().parse::<i32>().unwrap());
+    let result = numbers.fold(CalibrationAcc::Empty, |acc, current| {
+        match acc {
+            CalibrationAcc::Empty => CalibrationAcc::Full(current, 0),
+            CalibrationAcc::Full(old, amount) => CalibrationAcc::Full(
+                current,
+                if current > old { amount + 1 } else { amount },
+            )
+        }
+    });
+    match result {
+        CalibrationAcc::Empty => { panic!("Empty input!") }
+        CalibrationAcc::Full(_, amount) => { amount }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fluent_asserter::prelude::*;
+    use indoc::indoc;
+    use super::*;
+
+    const INPUT: &str = indoc! {"
+        199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
+    "};
+
+    #[test]
+    fn calibrate_correct() {
+        let result = calibrate(INPUT.lines());
+        assert_that!(result).is_equal_to(7);
+    }
+
+    #[test]
+    fn calibrate_empty() {
+        let result = || calibrate("".lines());
+        assert_that_code!(result).panics().with_message("Empty input!")
+    }
+}
